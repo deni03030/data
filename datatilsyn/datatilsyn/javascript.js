ac393b2b86734da4e6a070f1a614c54d7661ee07
deni03030 <102724084+deni03030@users.noreply.github.com>
2022-11-06 13:14:55 +0100

    Add files via upload

---
+++ datatilsyn/datatilsyn/javascript.js
@@ -0,0 +1,15 @@
+function tak() {
+  alert("Husk at svare på alle spørgesmål!");
+}
+
+function svar() {
+  const q1 = document.getElementsByName("input");
+  console.log(q1.inner);
+}
+
+/*function validateForm(event) {
+  event.preventDefault();
+  console.log(qestuion1.value);
+  const elements = document.getElementsByClassName("need-validation");
+  console.log(elements);
+} */
